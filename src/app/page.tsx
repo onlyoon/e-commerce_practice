b96c9a9218109a4196f5f348debf7511c217d4e0
SANGYOON PARK <parkyoon97@gmail.com>
2024-05-14 14:32:38 +0900

    init: nextjs template

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image';
+
+export default function Home() {
+  return (
+    <div className="bg-slate-50">
+      <section>
+        <MaxWidthWrapper></MaxWidthWrapper>
+      </section>
+    </div>
+  );
+}
